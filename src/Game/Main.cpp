ffd5fc3716ba29e157a672dd1be3866d78b83d2c
AminerZ <aminerthegamer2000@gmail.com>
2019-06-26 00:02:19 +0430

    Update Main.cpp
    Update Include path for States
    Add `initializeStates(StateHandler* stateHandler)`
    add `using namespace ExiledHeroes`

--- src/Game/Main.cpp
+++ src/Game/Main.cpp
@@ -1,17 +1,33 @@
 #include <memory>
 
 #include "Core/Game.h"
-#include "Game/States/States.h"
+#include "Game/State/States.h"
 
 using namespace ExiledHeroes;
 
+void initializeStates(std::unique_ptr<StateHandler>&& stateHandler);
+
 /* Main Entry Point of the Game */
 int main(int argc, char* argv[]) {
-	std::unique_ptr<ExiledHeroes::Game> game = 
-		std::make_unique<ExiledHeroes::Game>(argc, argv);
 	
-	auto& stateHandler = game->getHandlers().stateHandler;
 	
+	std::unique_ptr<Game> game = std::make_unique<Game>(argc, argv);
+
+	initializeStates(game->getStateHandler());
+
 	game->run();
 	return 0;
+}
+
+void initializeStates(std::unique_ptr<StateHandler>&& stateHandler) {
+	stateHandler->add<LoadState>(States::Load);
+	stateHandler->add<TestState>(States::Test);
+	// TODO: add more States Here
+	
+	LoadState::LoadStat loadStat(States::Test);
+	loadStat.textures = { {"TestAtlas", "./res/TestAtlasTile.png"} };
+	stateHandler->get<LoadState>(States::Load)
+		->setStat(loadStat);
+	
+	stateHandler->setState(States::Load);
 }
